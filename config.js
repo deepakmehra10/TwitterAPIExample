9e1b18c970993d9a5c10203e10f5326c8c509ede
Deepak Mehra <deeps.deeps1991@gmail.com>
2017-01-26 12:55:46 +0530

    Updated config js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
- consumer_key:         'oBAXeKQffPLOEtW1kv8jDE7bd',
-  consumer_secret:      'bGpDAH3y3t5PjMixzdoEfvx0sjvLjpJnX7JmJgCTDgzyAeLUv7',
-  access_token:         '104384669-YLIpqxmJyE92Vqzi60B8oZps9kbHevnWfVnyJC9m',
-  access_token_secret:  '9VtsJY1f2Oiu4jUPMHzx7c25GTsjuYxCsx1ygGdACLVhh'
+ consumer_key:         'YOUR_KEY',
+  consumer_secret:      'YOUR_SECRET_KEY',
+  access_token:         'ACCESS_TOKEN',
+  access_token_secret:  'ACCESS_TOKEN_SECRET'
 }
